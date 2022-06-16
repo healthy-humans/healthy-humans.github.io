9025718771c07e5b7a6bd8d798a3b6a7746945be
Brian Law <gmbrianlaw@gmail.com>
2022-06-15 22:36:13 -0700

    Use database models

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
 
     import FoodItem from './components/FoodItem.svelte';
+    import { NutritionValues } from './lib/database';
+    import { database } from './lib/database';
+
+    database.nutritionValues.count().then(
+        (count: number): void => {
+            if (!count) {
+                database.nutritionValues.add(new NutritionValues(300, 300, 60, 150, 2e3), 1);
+            }
+        }
+    );
 
     FoodItem;
 
